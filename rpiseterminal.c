2a911548585c2d13fbd8a9d2e39fe9b1b0fa935e
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-10-19 13:39:46 +0530

    updated rpiseterminal.c
    included rpiseterminal.h

--- rpiseterminal.c
+++ rpiseterminal.c
@@ -1,6 +1,6 @@
 #include<time.h>
+#include "rpiseterminal.h"
 
-enum Color { black='0',red='1',green='2',yellow='3',blue='4',magenta='5',cyan='6',white='7'};
 
 void eraseDisplay(){
    putchar(27);putchar('[');putchar('2');putchar('J');
