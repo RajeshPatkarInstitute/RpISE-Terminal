f9ebbcc471438274c2cd85e5e5e8c1cc49499d7f
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-10-19 13:38:22 +0530

    creating rpiseterminal.h
    version 0.1

---
+++ rpiseterminal.h
@@ -0,0 +1,10 @@
+enum Color { black='0',red='1',green='2',yellow='3',blue='4',magenta='5',cyan='6',white='7'};
+void eraseDisplay();
+void show(char ch);
+void hide();
+void hideCursor();
+void showCursor();
+void setForegroundColor(Color foreground);
+void setBackgroundColor(Color background);
+void sleep (unsigned int secs);
+void gotoxy(int x, int y);
