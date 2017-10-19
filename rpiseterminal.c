69653e009d30cba0f22b5283800b596e56852b94
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-10-19 13:34:19 +0530

    rpiseterminal v 0.1
    First working code. Still needs polishing and enhancements.

---
+++ rpiseterminal.c
@@ -0,0 +1,66 @@
+#include<time.h>
+
+enum Color { black='0',red='1',green='2',yellow='3',blue='4',magenta='5',cyan='6',white='7'};
+
+void eraseDisplay(){
+   putchar(27);putchar('[');putchar('2');putchar('J');
+   gotoxy(1,1);
+}
+
+void show(char ch){ 
+    putchar(ch); 
+    fflush(stdout); 
+}
+
+void hide(){
+   putchar(' ');
+   fflush(stdout);
+}
+
+void hideCursor(){
+   char es[6] = {27,'[','?','2','5','l'};
+   for(int i = 0; i <= 5; i++) 
+       putchar(es[i]); 
+}
+
+void showCursor(){
+   char es[6] = {27,'[','?','2','5','h'};
+   for(int i = 0; i <= 5; i++) 
+       putchar(es[i]); 
+}
+
+void setForegroundColor(Color foreground){
+   char es[5] = {27,'[','3',foreground,'m'};
+   for(int i = 0; i < 5; i++)
+       putchar(es[i]);
+}
+
+void setBackgroundColor(Color background){
+    putchar(27);putchar('[');putchar('4');putchar(background);putchar('m');
+    char es[5] = {27,'[','4',background,'m'};
+   for(int i = 0; i < 5; i++)
+       putchar(es[i]);
+}
+
+void sleep (unsigned int secs) {
+   unsigned int retTime = time(0) + secs;
+   while (time(0) < retTime);
+}
+
+void gotoxy(int x, int y){
+   char xstr[3]={'0','0','\0'};
+   char ystr[3]={'0','0','\0'}; 
+   sprintf(xstr,"%d",x);
+   sprintf(ystr,"%d",y);
+   putchar(27);putchar('[');
+   int i = 0 ;
+   for(;i<3&&ystr[i]!='\0';i++){
+       putchar(ystr[i]);
+   }
+   putchar(';');
+   i = 0 ;
+   for(;i<3&&xstr[i]!='\0';i++){
+       putchar(xstr[i]);
+   }
+   putchar('H');
+}
