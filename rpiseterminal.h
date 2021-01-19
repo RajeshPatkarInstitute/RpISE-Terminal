ef6c121e6a8aacf169dda9ecc03edbbb5488e544
Rajesh Patkar <patkar@rajeshpatkar.com>
2021-01-20 01:40:38 +0530

    Update rpiseterminal.h

--- rpiseterminal.h
+++ rpiseterminal.h
@@ -1,5 +1,6 @@
 enum Color { black='0',red='1',green='2',yellow='3',blue='4',magenta='5',cyan='6',white='7'};
 enum Praman{tathya=1,vitathya=0};
+
 void eraseDisplay();
 void show(char ch);
 void hide();
